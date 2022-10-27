723f1db17abd11fc2bb098fd6e25890b8ba8d93b
okanihbos <kalokaradya@gmail.com>
2022-10-27 14:57:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Const
+const footLove = document.querySelector("footer p i");
+
+// Function
+// * Kosong *
+
+// Event
+footLove.addEventListener("click", function () {
+    footLove.classList.toggle("text-primary");
+});
